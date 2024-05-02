1594e62cbbe64763d4d8d585b7124550974479ae
Tyee <flin00806@gmail.com>
2024-05-02 16:38:27 +0800

    init: 补充

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # lib-types
-hwjs世界依赖库的类型声明
+
+hwjs 世界依赖库的类型声明
+
+## 推荐使用方法
+
+作为 git submodule clone 到你的项目中
---
+++ index.d.ts
@@ -0,0 +1,44 @@
+/**
+ * Window对象上挂载的东东!
+ */
+export declare interface Window {
+    HWJS: HWJS;
+}
+/**
+ * 使用 hwjs API,写成函数调用的形式是因为某些API需要正确的执行上下文
+ * @returns hwjs API
+ */
+export declare const useHWJS: () => {
+    /**
+     * 获取用户信息
+     * @returns 用户信息的只读代理对象
+     */
+    getUser: () => User;
+    /**
+     * solidjs-toast消息条实例
+     */
+    toast: object;
+    /**
+     * modal实例
+     */
+    modal: {
+        open: (content: string) => void;
+        close: () => void;
+    };
+};
+
+/**
+ * HWJS
+ */
+export declare type HWJS = ReturnType<typeof useHWJS>;
+
+/**
+ * 用户信息
+ */
+declare interface User {
+    id: string;
+    username: string;
+    role: "admin" | "common";
+    released_worlds: string[];
+    favorite_worlds: string[];
+}
